25e21edbafce6cdb2d3b0126e6f319e463b9f1d0
Piotr Zoch <piotr.zoch@gmail.com>
2024-05-27 16:47:23 +0200

    season

--- code/arima/arma4.R
+++ code/arima/arma4.R
@@ -15,11 +15,12 @@ require(lubridate)
 
 datasetPi <- get_eurostat("prc_hicp_manr")
 
-tempPi <-subset(datasetPi, geo == c("PL") & coicop=="CP00" & unit=="RCH_A") 
+tempPi <- subset(datasetPi, geo == c("PL") & coicop=="CP00" & unit=="RCH_A") 
 tempPi <- na.omit(ts(rev(tempPi$values),start = c(1997, 1), frequency =12))
 
 
 Pi   <- window(tempPi,start = c(2002, 1), end = c(2019, 12))
+
 
 data_training.Pi <- window(Pi, end = c(2018, 12))
 data_testing.Pi <- window(Pi, start = c(2019, 1))
@@ -34,19 +35,26 @@ autoplot(Pi)
   acf(data_training.Pi,lag.max=24) 
   pacf(data_training.Pi,lag.max=24)
   df_test <- ur.df(data_training.Pi, type="none", lags=4)
-
+  
+  
+  
   # is there a problem?
-  model  <- arima(data_training.Pi, order = c(2,0,0), method = "ML")
+  model  <- arima(data_training.Pi, order = c(1,1,2), method = "ML")
+  
+  data_training.dPi = diff(data_training.Pi)
+  par(mfrow=c(1,2))
+  acf(data_training.dPi,lag.max=24) 
+  pacf(data_training.dPi,lag.max=24)
   
   # more specifications
-  model_plusar  <- arima(data_training.Pi, order = c(3,0,0), method = "ML")
-  model_plusma  <- arima(data_training.Pi, order = c(2,0,1), method = "ML")
+  model_plusar  <- arima(data_training.Pi, order = c(2,1,2), method = "ML")
+  model_plusma  <- arima(data_training.Pi, order = c(1,1,3), method = "ML")
   coeftest(model)
   coeftest(model_plusar)
   coeftest(model_plusma)
   
   resids <- model$residuals
-  Box.test(resids,lag=24, type="Ljung-Box",fitdf = 2 + 0 )
+  Box.test(resids,lag=24, type="Ljung-Box",fitdf = 2 + 1 )
   checkresiduals(model)
   
   
@@ -58,8 +66,8 @@ autoplot(Pi)
   
 # try with AIC etc
   # information criteria
-  model_aic = auto.arima(data_training.Pi,max.d=0,max.p=24,max.q=24,ic="aic",seasonal = FALSE,stepwise=FALSE, approximation=FALSE)
-  model_bic = auto.arima(data_training.Pi,max.d=0,max.p=24,max.q=24,ic="bic",seasonal = FALSE,stepwise=FALSE, approximation=FALSE)
+  model_aic = auto.arima(data_training.Pi,max.d=1,max.p=24,max.q=24,ic="aic",seasonal = FALSE,stepwise=FALSE, approximation=FALSE)
+  model_bic = auto.arima(data_training.Pi,max.d=1,max.p=24,max.q=24,ic="bic",seasonal = FALSE,stepwise=FALSE, approximation=FALSE)
 
   checkresiduals(model_aic)
   checkresiduals(model_bic)
@@ -67,8 +75,8 @@ autoplot(Pi)
   
   
 # general to specific 
-  model_general   <- arima(data_training.Pi, order = c(3,0,1), method = "ML")
-  model_specific  <- arima(data_training.Pi, order = c(2,0,0), method = "ML")
+  model_general   <- arima(data_training.Pi, order = c(1,1,2), method = "ML")
+  model_specific  <- arima(data_training.Pi, order = c(1,1,0), method = "ML")
   lgen = logLik(model_general)
   lspec =logLik(model_specific)
   dfs <- length(coef(model_general)) - length(coef(model_specific))
